81402da2fdbd0eaa1d4fcdbe5d39dec9fd5465d5
vritant24 <vritoku@gmail.com>
2017-06-13 20:06:48 +0530

    added basic functions for DotsAndBoxes

--- src/components/DotsAndBoxes.jsx
+++ src/components/DotsAndBoxes.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Grid from './Grid.jsx';
 
 export default class DotsAndBoxes extends Component {
   constructor(props) {
@@ -6,10 +7,31 @@ export default class DotsAndBoxes extends Component {
     this.state = {
       currentPlayer: 1,
       numPlayers: 2,
-      score: []
+      score: [],
+      gridSize: {
+        row: 10,
+        col: 10
+      }
     };
   }
+  nextPlayer() {
+    var nextPlayer = 1 + ((this.state.currentPlayer + 1) % this.state.numPlayers)
+    this.setState({
+      currentPlayer: nextPlayer;
+    });
+  }
+  incrementScore(player) {
+    var _score = this.state.score.slice();
+    _score[player]++;
+    this.setState({
+      score: _score;
+    });
+  }
   render() {
-
+    return(
+      <div>
+        <Grid nextPlayer={this.nextPlayer.bind(this)} incrementScore={this.incrementScore.bind(this)} gridSize={this.state.gridSize} numPlayers={this.state.numPlayers}/>
+      </div>
+    );
   }
 }
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 export default class Grid extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      playgrid: this.setUpPlayGrid(),
+      currentPlayer: 1
+    }
+    this.gridSize = props.gridSize;
+    this.numPlayers = this.numPlayers;
   }
   render() {
 
